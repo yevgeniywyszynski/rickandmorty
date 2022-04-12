78782c13746e89d8893ab53ae13b9863634adfe6
Yevgeniy Wyszyński <yevgeniywyszynski@Yevgeniys-MacBook-Pro.local>
2022-04-12 11:39:12 +0200

    Add request thunk

--- src/redux/AllHerosRedux.js
+++ src/redux/AllHerosRedux.js
@@ -1,4 +1,50 @@
 
+import axios from 'axios';
+
+export const getAllHeros = ({allHeros}) => allHeros;
+
+const reducerName = 'heros'
+
+const createActionName = name => `app/${reducerName}/${name}`;
+
+const START_REQUEST = createActionName('START_REQUEST');
+const END_REQUEST = createActionName('END_REQUEST');
+const ERROR_REQUEST = createActionName('ERROR_REQUEST');
+
+const LOAD_ALLHEROS = createActionName('LOAD_ALLHEROS');
+
+export const startRequest = () => ({type: START_REQUEST});
+export const endRequest = () => ({type: END_REQUEST});
+export const errorRequest = payload => ({payload, type: ERROR_REQUEST});
+
+export const loadAllHeros = payload => ({payload, type: LOAD_ALLHEROS});
+
+export const loadAllHerosRequest = () => {
+    return async dispatch => {
+        dispatch(startRequest());
+
+        try {
+            let allHeros = await axios.get('https://rickandmortyapi.com/api/character')
+            dispatch(loadAllHeros(allHeros.data));
+            dispatch(endRequest())
+        } catch(e) {
+            dispatch(errorRequest({name: 'ERROR_REQUEST', error: 'could not fetch data'}));
+        }
+    }
+}
+
+
 export default function reducer(statePart = [], action = {}) {
-    return statePart
+    switch(action.type) {
+        case LOAD_ALLHEROS:
+            return {...statePart, data: action.payload}
+        case START_REQUEST:
+            return {...statePart, request: {pending: true, error: null, success: false}}
+        case END_REQUEST: 
+            return {...statePart, request: {pending: false, error: null, success: true}}
+        case ERROR_REQUEST: 
+            return {...statePart, request: { pending: false, error: action.error, success: false}}
+        default:
+            return statePart
+    }
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -16,6 +16,7 @@ const storeReducer = combineReducers(reducers);
 const store = createStore (
     storeReducer,
     initalState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
 export default store
