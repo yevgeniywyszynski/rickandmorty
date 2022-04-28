817ca9f7b12a2760d4a82f90c089be7144292293
Your Name <you@example.com>
2022-04-28 11:02:25 +0200

    Change webpack

--- src/redux/store.js
+++ src/redux/store.js
@@ -21,7 +21,7 @@ const storeReducer = combineReducers(reducers);
 const store = createStore (
     storeReducer,
     initalState,
-    compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+    compose(applyMiddleware(thunk))
 )
 
 export default store
